112fe2842ee777d4c00c216d65b8623d2015365a
DmitriySaburov <dmitriy.saburov94@mail.ru>
2025-03-18 14:29:13 +0300

    изменил main_file.py

--- main_file.py
+++ main_file.py
@@ -5,4 +5,13 @@ for i in range(1, 4):
     sleep(1)
 
 print("Hello, World!!!")
-sleep(3)
+sleep(1)
+
+print("это еще не всё")
+
+for i in range(1, 4):
+    print(i)
+    sleep(1)
+
+print("теперь всё")
+sleep(1)
