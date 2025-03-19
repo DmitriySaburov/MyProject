9d3ac07090d029677f06faa22bd72cb8e9d6c3fd
DmitriySaburov <dmitriy.saburov94@mail.ru>
2025-03-19 13:57:55 +0300

    добавил декоратор

--- main_file.py
+++ main_file.py
@@ -1,4 +1,5 @@
 from time import sleep
+from functools import wraps
 
 for i in range(1, 3):
     print(i)
@@ -8,3 +9,22 @@ print("Hello, World!!!")
 sleep(1)
 
 print("добавили еще")
+
+def my_dec(number: int):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            print(f"начало работы декоратора {number}")
+            result = func(*args, **kwargs)
+            print(f"конец работы декоратора {number}")
+            return result
+        return wrapper
+    return decorator
+            
+
+@my_dec(5)
+def func(value: int) -> int:
+    print(f"работает функция: {func.__name__}")
+    return value ** 2
+
+print(func(5))
