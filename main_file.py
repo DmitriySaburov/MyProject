46ef8b5fb9cd44fb54a19d9334d49c77b956e182
DmitriySaburov <dmitriy.saburov94@mail.ru>
2025-03-20 16:57:01 +0300

    обновил полностью main_file.py убрал всё лишнее

--- main_file.py
+++ main_file.py
@@ -1,18 +1,10 @@
 from time import sleep
 from functools import wraps
 
-for i in range(1, 3):
-    print(i)
-    sleep(1)
-
-print("обновление другим разработчиком")
-"""еще добавили другие разработчики"""
-
 print("Hello, World!!!")
 sleep(1)
 
-print("добавили еще")
-
+# декоратор
 def my_dec(number: int):
     def decorator(func):
         @wraps(func)
@@ -24,21 +16,15 @@ def my_dec(number: int):
         return wrapper
     return decorator
             
-
 @my_dec(5)
 def func(value: int) -> int:
     print(f"работает функция: {func.__name__}")
     return value ** 2
 
-if __name__ == "__main__":
-    result = func(value=10)
-    print(result)
 
 def my_func(string: str) -> int:
     """возвращает кол-во гласных букв в строке"""
     predicate = lambda w: w in "aeyuio"
     return len(list(filter(predicate, string)))
 
-print("первый коммит")
-
-print("второй коммит")
+print("конец")
